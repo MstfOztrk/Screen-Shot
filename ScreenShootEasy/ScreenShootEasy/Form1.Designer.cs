f9f4c37aea9e3a6d4637949e64673f87cb9a4c2b
Mustafa Öztürk <58623025+MstfOztrk@users.noreply.github.com>
2021-03-24 01:28:21 +0300

    Screenshot app but has slight problems
    memory usage needs to be reduced

---
+++ ScreenShootEasy/ScreenShootEasy/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ScreenShootEasy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ScreenShot = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ScreenShot
+            // 
+            this.ScreenShot.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.ScreenShot.BalloonTipText = "Press PrtScSysRq Key";
+            this.ScreenShot.BalloonTipTitle = "Screen Shot ";
+            this.ScreenShot.Icon = ((System.Drawing.Icon)(resources.GetObject("ScreenShot.Icon")));
+            this.ScreenShot.Text = "ScreenShot";
+            this.ScreenShot.Visible = true;
+            this.ScreenShot.DoubleClick += new System.EventHandler(this.ScreenShot_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 62);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "ScreenShot";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon ScreenShot;
+    }
+}
+
---
+++ ScreenShootEasy/ScreenShootEasy/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ScreenShootEasy
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //DLL for keyboard listen
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        //Loop start
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        Bitmap captureBitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+        Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+        
+        //Taking Screen Shot and Save image
+        void GetScreenShot()
+        {
+            
+            try
+            {
+                Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+                captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+                captureBitmap.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"\\ScreenShot.Png", ImageFormat.Png);             
+                captureGraphics.Dispose();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }   
+
+        //Loop and keyboard listener
+        private void timer1_Tick(object sender, EventArgs e)
+        {   //Read ASCII
+            for (byte i = 32; i < 127; i++)
+            {   //Read Necessary code
+                if ((int)GetAsyncKeyState(i) == 32768)
+                {   //Listen PrtScSysRq Key only and translate ASCII
+                    if((char)i==',')
+                    {
+                        GetScreenShot();
+                    }
+                   
+                }
+            }         
+        }
+
+        private void ScreenShot_DoubleClick(object sender, EventArgs e)
+        {   //notify for close program
+            Application.Exit();
+        }
+    }
+
+  
+}
+
